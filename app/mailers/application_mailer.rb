72f3eb79c71a330a5e2e3219dded4314675f3780
marcelfonteles <marcelfonteles@gmail.com>
2018-02-05 13:26:05 -0300

    chaves AWS

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   include SendGrid if Rails.env.production?
-  default :from => %{"EGEJ 18" <marcelfonteles@gmail.com>}
+  default :from => %{"EGEJ 18" <experiencia@fejers.org.br>}
   layout 'mailer'
 end
 
