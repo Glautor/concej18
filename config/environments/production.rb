e7bbf8363ff3441a4121092b063945236a4d7011
marcelfonteles <marcelfonteles@gmail.com>
2018-02-19 11:00:21 -0300

    configurado production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'LINK DA TELA DE LOGIN' }
+  config.action_mailer.default_url_options = { :host => 'egej18.herokuapp.com' }
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   :email => {
