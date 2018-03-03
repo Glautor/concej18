5c634c120d67578e881c0211b5a8d9c1293d329f
marcelfonteles <marcelfonteles@gmail.com>
2018-03-02 23:49:18 -0300

    google-keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'Google',                        # required
-    aws_access_key_id:     Rails.application.secrets.access_key_id,            # required
-    aws_secret_access_key: Rails.application.secrets.secret_access_key,            # required
+    aws_access_key_id:     Rails.application.google_storage_secrets.access_key_id,            # required
+    aws_secret_access_key: Rails.application.google_storage_secrets.secret_access_key,            # required
   }
   config.fog_directory  = 'egej18'                          # required
 end
