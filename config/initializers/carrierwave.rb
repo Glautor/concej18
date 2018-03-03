124800169240ba1815671351c7b52817e8766743
marcelfonteles <marcelfonteles@gmail.com>
2018-03-03 15:19:58 -0300

    google-keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,9 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/google'
+  config.fog_provider = 'fog/google'                        # required
   config.fog_credentials = {
-<<<<<<< HEAD
     provider:                         'Google',
-    google_storage_access_key_id:     Rails.application.secrets.access_key_id,
-    google_storage_secret_access_key: Rails.application.secrets.secret_access_key
+    google_storage_access_key_id:     Rails.application.secrets.google_storage_access_key_id,
+    google_storage_secret_access_key: Rails.application.secrets.google_storage_secret_access_key
   }
-  config.fog_directory  = 'egej18'                          # required
-  config.fog_public     = true                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-=======
-    provider:              'Google',                        # required
-    google_storage_access_key_id:     Rails.application.secrets.google_storage_access_key_id,            # required
-    google_storage_secret_access_key: Rails.application.secrets.google_storage_secret_access_key,            # required
-  }
-  config.fog_directory  = 'egej18'                          # required
->>>>>>> google-keys
+  config.fog_directory = 'egej18'
 end
