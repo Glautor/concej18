bb7787fd91e097213e1e2606b7e8e11a36b18810
Glauton <glautoncardoso@gmail.com>
2018-05-26 03:19:36 +0000

    mudar nome da ej com before_action(teste)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def user_must_have_changed_ej
     if @user.junior_enterprise == "Nome da sua EJ"
-      redirect_to "/inscription/edit#nome_da_ej", alert: "Cadastre o nome da sua EJ conforme o aviso na tela inicial especifica"
+      redirect_to "/inscription/edit#nome_da_ej", alert: "Cadastre o nome da sua EJ"
     end
   end
 
